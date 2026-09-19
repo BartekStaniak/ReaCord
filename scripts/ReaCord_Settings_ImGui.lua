8ae9355bb13740d23bffae39ad7089a666fd3996
Bartek <exbrosjr@gmail.com>
2026-09-19 20:10:46 +0100

    release: v1.0.2 - logo customisation (REAPER classic vs ReaCord hybrid)

--- scripts/ReaCord_Settings_ImGui.lua
+++ scripts/ReaCord_Settings_ImGui.lua
@@ -1,6 +1,6 @@
 -- @description ReaCord Settings (ReaImGui Modern Interface)
 -- @author Bartek Staniak
--- @version 1.0.2-beta1
+-- @version 1.0.2
 -- @about
 --   Modern hardware-accelerated GUI for ReaCord with live Discord profile card preview.
 --   Provides real-time configuration of privacy opt-ins and presence attributes.
